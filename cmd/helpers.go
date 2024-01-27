b0bdd9d96b752d6962e9263857aa14d3b21ba5a7
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-01-27 21:53:40 +0530

    Refactor `getClock` function to simplify time formatting

--- cmd/helpers.go
+++ cmd/helpers.go
@@ -2,25 +2,12 @@ package cmd
 
 import (
 	"fmt"
-	"strconv"
 	"time"
 )
 
 // Returns the current time in the format "HH:MM:SS"
 func getClock(now time.Time) (hours, minutes, seconds string) {
-	h, m, s := now.Clock() //	Get hours, minutes and seconds
-
-	//	Format hours, minutes and seconds
-	hours, minutes, seconds = strconv.Itoa(h), strconv.Itoa(m), strconv.Itoa(s)
-	if len(hours) == 1 {
-		hours = fmt.Sprintf("0%s", hours)
-	}
-	if len(minutes) == 1 {
-		minutes = fmt.Sprintf("0%s", minutes)
-	}
-	if len(seconds) == 1 {
-		seconds = fmt.Sprintf("0%s", seconds)
-	}
-
-	return hours, minutes, seconds
+	return fmt.Sprintf("%02d", now.Hour()),
+		fmt.Sprintf("%02d", now.Minute()),
+		fmt.Sprintf("%02d", now.Second())
 }

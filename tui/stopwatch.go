fd2a289b8d969137f47bdc22ce394126eb959d2c
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-01-28 01:32:11 +0530

    Update Comments

--- tui/stopwatch.go
+++ tui/stopwatch.go
@@ -22,16 +22,19 @@ func tick() tea.Cmd {
 // MODEL
 // -----
 
+// Represents a stopwatch
 type stopwatchModel struct {
-	running bool
-	start   time.Time
-	end     time.Time
-	elapsed time.Duration
+	running bool          // Whether the stopwatch is running
+	start   time.Time     // The time the stopwatch was started
+	end     time.Time     // The time the stopwatch was stopped
+	elapsed time.Duration // The elapsed time
 
-	keys KeyMap
-	help help.Model
+	keys KeyMap     // Key bindings for the stopwatch view
+	help help.Model // Help menu model
 }
 
+// Creates a new stopwatch model. Initializes the default key bindings and
+// help menu. The stopwatch is "stopped" by default.
 func NewStopwatchModel() *stopwatchModel {
 	return &stopwatchModel{
 		keys: DefaultKeyMap,
@@ -42,42 +45,52 @@ func NewStopwatchModel() *stopwatchModel {
 // INIT, UPDATE, VIEW
 // ------------------
 
+// The Init function is called when the program starts and returns a command
+// to run immediately. We'll start the stopwatch ticker.
 func (m *stopwatchModel) Init() tea.Cmd {
 	return tick()
 }
 
+// The Update function is called when events happen, like keypresses
 func (m *stopwatchModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 
+	// Window Resize Event
 	case tea.WindowSizeMsg:
 		// If we set a width on the help menu it can gracefully truncate
 		// its view as needed.
 		m.help.Width = msg.Width
 
+	// Stopwatch Start
 	case msgStart:
 		m.start = msg.t
 		m.running = true
 		m.keys.Space.SetHelp("<spacebar>", "Stop")
 		return m, nil
 
+	// Stopwatch Stop
 	case msgStop:
 		m.end = time.Now()
 		m.running = false
 		m.keys.Space.SetHelp("<spacebar>", "Start")
 		return m, nil
 
+	// Stopwatch Tick
 	case msgTick:
 		if m.running {
 			m.elapsed = time.Since(m.start)
 		}
 		return m, tick()
 
+	// Key Press
 	case tea.KeyMsg:
 		switch {
 
+		// Quit
 		case key.Matches(msg, DefaultKeyMap.Quit):
 			return m, tea.Quit
 
+		// Space
 		case key.Matches(msg, DefaultKeyMap.Space):
 			if m.running {
 				return m, m.Stop
@@ -92,6 +105,7 @@ func (m *stopwatchModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	return m, nil
 }
 
+// The View function is responsible for rendering the UI.
 func (m *stopwatchModel) View() string {
 	s := "\n"
 
@@ -108,6 +122,7 @@ func (m *stopwatchModel) View() string {
 // COMMAND
 // -------
 
+// A command to start the stopwatch
 func ShowStopwatch() {
 	p := tea.NewProgram(NewStopwatchModel())
 	if _, err := p.Run(); err != nil {

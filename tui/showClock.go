5ff13398ff17c26656a6c7be08a6291105f8f696
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-01-27 23:15:11 +0530

    Add date and padding to showClock view

--- tui/showClock.go
+++ tui/showClock.go
@@ -70,6 +70,12 @@ func (c *ClockModel) View() string {
 	hours, minutes, seconds := c.formatTime()
 	s += " " + hours + ":" + minutes + ":" + seconds + "\t"
 
+	// add the date in the format "Saturday, 27 January 2024"
+	s += c.t.Format("Monday, 2 January 2006")
+
+	// add a newline and some padding at the bottom
+	s += "\n\n"
+
 	return s
 }
 

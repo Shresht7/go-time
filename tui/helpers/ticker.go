3a502bf1bc6d90dc8756cc8c2f480a6579d41e6d
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-01-28 15:17:13 +0530

    Create `helpers` package

---
+++ tui/helpers/ticker.go
@@ -0,0 +1,23 @@
+package helpers
+
+import (
+	"time"
+
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+// MESSAGE
+// -------
+
+// MsgTick is the message sent by our ticker to update the clock
+type MsgTick struct{ t time.Time }
+
+// COMMAND
+// -------
+
+// ticker is a helper function that returns a tea.Cmd that sends a MsgTick every interval
+func CmdTicker(interval time.Duration) tea.Cmd {
+	return tea.Tick(interval, func(t time.Time) tea.Msg {
+		return MsgTick{t}
+	})
+}

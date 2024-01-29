0fd900d92fcb5f8d7936aa4d868f0c8b4aaee7b3
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-01-30 01:18:05 +0530

    Refactor list view in timer component

--- tui/timer/components/list/view.go
+++ tui/timer/components/list/view.go
@@ -5,24 +5,35 @@ import "github.com/charmbracelet/lipgloss"
 // VIEW
 // ----
 
-var borderStyle = lipgloss.NewStyle().Border(lipgloss.RoundedBorder(), true)
+var borderStyle = lipgloss.NewStyle().Border(lipgloss.RoundedBorder(), true).Padding(1, 2)
 var focusedStyle = borderStyle.Copy().BorderForeground(lipgloss.Color("12"))
 var unfocusedStyle = borderStyle.Copy().BorderForeground(lipgloss.Color("240"))
 
 func (m Model) View() string {
-	var s string
-	if m.prompt != "" {
-		s += m.prompt + "\n"
-	}
-	for i, item := range m.items {
-		if i == m.selected {
-			s += " ▶ " + item + "\n"
-		} else {
-			s += "   " + item + "\n"
-		}
-	}
+
+	s := lipgloss.JoinVertical(lipgloss.Top,
+		m.ViewPrompt(),
+		m.ViewList(),
+	)
+
 	if m.isFocused {
 		return focusedStyle.Render(s)
 	}
 	return unfocusedStyle.Render(s)
+}
+
+func (m Model) ViewPrompt() string {
+	return lipgloss.NewStyle().Bold(true).Render(m.prompt)
+}
+
+func (m Model) ViewList() string {
+	var s string
+	for i, item := range m.items {
+		if i == m.selected {
+			s += "▶ " + item + "\n"
+		} else {
+			s += "  " + item + "\n"
+		}
+	}
+	return s
 }

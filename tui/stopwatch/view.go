255c16b2869bc498d69f6135b4d33f41ed6405dd
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-01-29 17:14:09 +0530

    Refactor stopwatch view rendering

--- tui/stopwatch/view.go
+++ tui/stopwatch/view.go
@@ -1,23 +1,19 @@
 package stopwatch
 
 import (
-	"github.com/charmbracelet/lipgloss"
-
 	"github.com/Shresht7/go-time/tui/helpers"
 )
 
 // VIEW
 // ----
 
-var styles = lipgloss.NewStyle().Padding(1, 0)
-
 // The View function is responsible for rendering the UI.
 func (m *stopwatchModel) View() string {
-	s := lipgloss.JoinVertical(lipgloss.Top, helpers.Filter(
-		styles.Render(helpers.FormatDuration(m.elapsed)),
+	s := helpers.FlexBoxColumn(helpers.Filter(
+		"\n"+helpers.FormatDuration(m.elapsed)+"\n",
 		m.laps.View(),
 		m.help.View(m.keys),
-	)...)
+	)...).String()
 
 	return s
 }

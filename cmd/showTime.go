470e988a95516fb15b1610fe3c00f428d9c2e846
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-01-31 00:36:31 +0530

    Remove flexbox layout package
    It wasn't doing anything too impressive

--- cmd/showTime.go
+++ cmd/showTime.go
@@ -5,14 +5,11 @@ import (
 	"time"
 
 	"github.com/Shresht7/go-time/pkg/format"
-	"github.com/Shresht7/go-time/pkg/layout"
 )
 
 // Shows the current time and date
 func ShowTime() {
 	now := time.Now()
-	result := layout.Column(
-		layout.Row(format.Icon(now), format.TimeAndDate(now)).WithGap(3).String(),
-	)
-	fmt.Println(result)
+	result := fmt.Sprintf("%s   %s", format.Icon(now), format.TimeAndDate(now))
+	fmt.Println("\n" + result + "\n")
 }
--- tui/stopwatch/view.go
+++ tui/stopwatch/view.go
@@ -1,8 +1,9 @@
 package stopwatch
 
 import (
+	"github.com/charmbracelet/lipgloss"
+
 	"github.com/Shresht7/go-time/pkg/format"
-	"github.com/Shresht7/go-time/pkg/layout"
 	"github.com/Shresht7/go-time/tui/helpers"
 )
 
@@ -11,11 +12,10 @@ import (
 
 // The View function is responsible for rendering the UI.
 func (m *stopwatchModel) View() string {
-	s := layout.Column(helpers.Filter(
+	s := lipgloss.JoinVertical(lipgloss.Left, helpers.Filter(
 		"\n"+format.Duration(m.elapsed)+"\n",
 		m.laps.View(),
 		m.help.View(m.keys),
-	)...).String()
-
+	)...)
 	return s
 }

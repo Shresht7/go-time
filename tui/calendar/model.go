ebfdbfd0a66d71f5fe170f1fa66299aa86e168a8
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-01-30 16:23:20 +0530

    Add keyboard navigation to calendar

--- tui/calendar/model.go
+++ tui/calendar/model.go
@@ -23,6 +23,24 @@ func (m Model) Init() tea.Cmd {
 }
 
 func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+
+	case tea.KeyMsg:
+		switch msg.String() {
+
+		case "left", "h":
+			m.t = m.t.AddDate(0, -1, 0)
+
+		case "right", "l":
+			m.t = m.t.AddDate(0, 1, 0)
+
+		case "q", "esc", "ctrl+c":
+			return m, tea.Quit
+
+		}
+
+	}
+
 	return m, nil
 }
 

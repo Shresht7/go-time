06c83475bf155887d3ce0224ba9ab6b1bfbff3b6
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-01-30 12:56:13 +0530

    Add help message to clock view

--- tui/clock/view.go
+++ tui/clock/view.go
@@ -1,6 +1,7 @@
 package clock
 
 import (
+	"github.com/Shresht7/go-cli-tools/ansi/styles"
 	"github.com/Shresht7/go-time/pkg/format"
 )
 
@@ -12,5 +13,7 @@ func (c *model) View() string {
 	icon := format.Icon(c.t)
 	timeAndDate := format.TimeAndDate(c.t)
 	result := icon + "   " + timeAndDate
+	help := styles.Faint("Press q to quit")
+	result += "\t\t" + help
 	return "\n" + result + "\n\n"
 }

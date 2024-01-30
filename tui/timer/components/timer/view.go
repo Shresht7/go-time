ca31a4daedd69bf51637340e90567d1d15acf821
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-01-30 10:47:25 +0530

    Refactor timer view styling

--- tui/timer/components/timer/view.go
+++ tui/timer/components/timer/view.go
@@ -1,20 +1,11 @@
 package timer
 
-import (
-	"github.com/charmbracelet/lipgloss"
-)
+import "github.com/charmbracelet/lipgloss"
 
 // VIEW
 // ----
 
-var borderStyle = lipgloss.NewStyle().Border(lipgloss.RoundedBorder(), true)
-var focusedStyle = borderStyle.Copy().BorderForeground(lipgloss.Color("12"))
-var unfocusedStyle = borderStyle.Copy().BorderForeground(lipgloss.Color("240"))
-
 func (m Model) View() string {
 	s := m.FormatTime()
-	if m.isFocused {
-		return focusedStyle.Render(s)
-	}
-	return unfocusedStyle.Render(s)
+	return lipgloss.NewStyle().Padding(1, 2).Render(s)
 }
